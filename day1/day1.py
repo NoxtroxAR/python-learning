9b25e73cde314c51efe5ea306b6b62b2e2303e9a
Aaryan Shrestha <stha.aaryan555@gmail.com>
2026-09-11 11:43:21 +0545

    Add files via upload
    Day 01 - Python basics

---
+++ day1/day1.py
@@ -0,0 +1,17 @@
+#print is a function to print
+print("Hello World")#"" cuz its a string
+print(7)
+print("Hello World",7)
+print("I like the number:",9,"Hello World")
+print(7*7)
+#escape seuqnce \n used to go to next line
+print("Hello World\nI like the number 9")
+'''
+this is multi line comment
+'''
+print("hey i am \"good boy\"\nI like the number 9")#"" can be inserted inside string through \ \
+print("hey",6,7,8,9,10,sep="*")#sep is used to separate the values with any 
+#output:hey*6*7*8*9*10
+print("hey",6,7,8,9,10,sep="*",end="\n67")#end is used to end the line with any character
+#output:hey*6*7*8*9*10
+#       67
