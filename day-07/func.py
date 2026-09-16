dc5a0f19a749e20325e66eba4cd05e6545f9d5c6
Aaryan Shrestha <stha.aaryan555@gmail.com>
2026-09-16 21:07:03 +0545

    Add files via upload

---
+++ day-07/func.py
@@ -0,0 +1,13 @@
+def calculate():
+    pass
+def calculate(a,b):
+    print(a+b)
+calculate(10,20)
+calculate(100,200)
+
+def calculate():
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    print(a+b)
+
+calculate()
