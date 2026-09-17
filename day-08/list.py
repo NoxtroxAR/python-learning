b070698d3ca3d5457e7dee0c788235da3dda6f3b
Aaryan Shrestha <stha.aaryan555@gmail.com>
2026-09-17 22:14:12 +0545

    Add files via upload

---
+++ day-08/list.py
@@ -0,0 +1,9 @@
+#list can contain of string int boolean all datatypes
+marks=[1,"Two",3]
+print(marks) #output: [1, 'Two, 3]
+print(marks[0]) #output: 1
+print(marks[1]) #output: Two
+#negative indexing
+print(marks[-1]) #output: 3
+print(marks[-2]) #output: Two
+#len(marks)-2=3-2 so it will print [1] index which is two
